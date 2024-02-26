1ea683cff7d98121f35d8112ea2e8f65379bd295
DoychinAlexandrov <doychin_alexandrov@abv.bg>
2024-02-26 15:43:16 +0200

    add all

--- app/components/JoinUs/JoinUs.tsx
+++ app/components/JoinUs/JoinUs.tsx
@@ -5,13 +5,13 @@ const JoinUs = () => {
   return (
     <>
         <div className='wrapper px-4 text-white'>
-            <div className='border-b border-solid border-white'>
+            <div className='border-t border-b border-solid pt-5 border-white'>
                  <h2 className='text-5xl md:text-6xl mb-28'>Join us in shaping the future of technology.</h2>
 
                    <Link href='' className='text-3xl w-full text-center border border-solid border-white rounded block py-4 hover:text-black hover:bg-white transition-all duration-300'>
                         View Careers
-                    </Link>
-                </div>
+                   </Link>
+             </div>
         </div>
     </>
   )
--- app/components/Testimonal/Testimonial.tsx
+++ app/components/Testimonal/Testimonial.tsx
@@ -6,16 +6,16 @@ const Testimonial = () => {
   return (
     <>
         <div className='wrapper bg-black text-white'>
-            <div className='grid grid-cols-1 md:grid-cols-2 px-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 px-4 pt-6 pb-40'>
                 <div className='md:order-2 mb-8'>
                      <Image src={imgPortrait}  alt="Image" className="w-full object-cover"/>
                 </div>
 
                 <div className='md:order-1'>
                     <blockquote className='mb-5 md:mb-10'>
-                        <p className = 'text-3xl md:text-5xl'>
+                        <p className = 'text-3xl md:text-4xl xl:text-5xl'>
                             I encourage my team to keep learning. Ideas in different topics or fields can often inspire new ideas and     broaden the potential solution space.
-                      </p>
+                        </p>
                     </blockquote>
 
                     <h4 className='font-bold'>Lilian Weng</h4>
